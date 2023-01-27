39789592f0afe300ad9334104a7d3ac318a457e8
Leonardo Miranda <leonardmiranda99@gmail.com>
2023-01-26 19:55:46 -0600

    correciones

--- api.js
+++ api.js
@@ -1,42 +1,49 @@
-// Obtener los datos de la API
-fetch("https://reqres.in/api/users?delay=3")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // Mostrar los datos en una tabla
-    let table = document.getElementById("tabla-de-datos");
-
-    for (let i = 0; i < data.data.length; i++) {
-      let row = table.insertRow();
-
-      let idCell = row.insertCell();
-      let nameCell = row.insertCell();
-      let emailCell = row.insertCell();
-      let avatarCell = row.insertCell();
-      let avatarImg = document.createElement("img");
-
-      idCell.innerHTML = data.data[i].id;
-      nameCell.innerHTML = data.data[i].first_name + " " + data.data[i].last_name;
-      emailCell.innerHTML = data.data[i].email;
-      avatarImg.src = data.data[i].avatar;
-      avatarImg.classList.add("round-img");
-      avatarCell.appendChild(avatarImg);
-    }
-  });
-
-// Almacenar datos del almacenamiento local
 let storedData = JSON.parse(localStorage.getItem("data"));
 if (storedData && storedData.expiration > Date.now()) {
-  console.log(storedData.data);
+    console.log(storedData.data);
+    let table = document.getElementById("tabla-de-datos");
+
+    for (let i = 0; i < storedData.data.data.length; i++) {
+        let row = table.insertRow();
+
+        let idCell = row.insertCell();
+        let nameCell = row.insertCell();
+        let emailCell = row.insertCell();
+        let avatarCell = row.insertCell();
+        let avatarImg = document.createElement("img");
+
+        idCell.innerHTML = storedData.data.data[i].id;
+        nameCell.innerHTML = storedData.data.data[i].first_name + " " + storedData.data.data[i].last_name;
+        emailCell.innerHTML = storedData.data.data[i].email;
+        avatarImg.src = storedData.data.data[i].avatar;
+        avatarImg.classList.add("round-img");
+        avatarCell.appendChild(avatarImg);
+    }
 } else {
- 
-  fetch("https://reqres.in/api/users?delay=3")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      let expirationTime = Date.now() + 4000;
-      localStorage.setItem("data", JSON.stringify({ data: data, expiration: expirationTime }));
-    });
+    setTimeout(() => {
+        fetch("https://reqres.in/api/users?page=1")
+            .then(response => response.json())
+            .then(data => {
+                let expirationTime = Date.now() + 60000;
+                localStorage.setItem("data", JSON.stringify({ data: data, expiration: expirationTime }));
+                let table = document.getElementById("data-table-body");
+
+                for (let i = 0; i < data.data.length; i++) {
+                    let row = table.insertRow();
+
+                    let idCell = row.insertCell();
+                    let nameCell = row.insertCell();
+                    let emailCell = row.insertCell();
+                    let avatarCell = row.insertCell();
+                    let avatarImg = document.createElement("img");
+
+                    idCell.innerHTML = data.data[i].id;
+                    nameCell.innerHTML = data.data[i].first_name + " " + data.data[i].last_name;
+                    emailCell.innerHTML = data.data[i].email;
+                    avatarImg.src = data.data[i].avatar;
+                    avatarImg.classList.add("round-img");
+                    avatarCell.appendChild(avatarImg);
+                }
+            });
+    }, 4000);
 }
